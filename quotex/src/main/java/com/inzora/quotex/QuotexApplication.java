955021d845c491bf5b56465bd801e0bf6c8acf18
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-11-01 07:44:40 -0300

    chore: init

---
+++ quotex/src/main/java/com/inzora/quotex/QuotexApplication.java
@@ -0,0 +1,13 @@
+package com.inzora.quotex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotexApplication.class, args);
+	}
+
+}
---
+++ quotex/src/main/java/com/inzora/quotex/enums/InvestmentProfile.java
@@ -0,0 +1,7 @@
+package com.inzora.quotex.enums;
+
+public enum InvestmentProfile {
+    CONSERVATIVE,
+    MODERATE,
+    AGGRESSIVE
+}
